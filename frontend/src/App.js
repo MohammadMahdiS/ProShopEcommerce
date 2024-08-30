8d34fdde5038c1930eceaaec0c116df916c38682
unknown <mahdi.soleimani70@gmail.com>
2024-08-30 04:26:11 -0700

    initiall commit:add proshop ui react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from './pages/Screen/HomeScreen';
+import {Routes, Route} from 'react-router-dom'
+import ProductScreen from './pages/Screen/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+      <main className="py-5">
+        <Container>
+        <Routes>
+          <Route path="/"  element={<HomeScreen />} exact/> 
+          <Route path="/product/:id"  element={<ProductScreen />} /> 
+    </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import Rating from './Rating'
+
+function Product({product}) {
+  return (
+    <Card className="my-3 p-3 rounded">
+      {/* Link to the product details page */}
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" /> {/* Fixed Card.Img */}
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`} className="text-decoration-none">
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div" className="my-3">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`} // Fixed property name to match product object
+            color={'#f8e825'}
+          />
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+
+        {/* Display product description if available */}
+        {product.description && (
+          <Card.Text as="p">
+            {product.description}
+          </Card.Text>
+        )}
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
